d39b935bfe4132a92950d6b68d75c163e720d276
Gray Smith <gray.smith00@gmail.com>
2019-01-09 20:19:19 +0100

    Add about us route

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ app.get('/', (req, res) => {
   res.json({ message: 'helloworld' });
 });
 
+app.get('/about', (req, res) => {
+  res.json({ message: 'hello from about us' });
+});
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`server running on port ${port}`);
